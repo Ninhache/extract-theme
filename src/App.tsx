cb635ba54c5922f2dcff2509a8069598a72d9cfa
Ninhache <mr.carambar27@gmail.com>
2024-11-10 11:55:01 +0100

    fix: improve landing page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ import { useImage } from "./contexts/ImageProvider";
 import LeftSide from "./LeftSide";
 import { createBlobURL, openFile } from "./lib/utils";
 import { DataContextProvider } from "./contexts/DataProvider";
+
 import Konvas from "./Konvas";
+import Balancer from "react-wrap-balancer";
 
 function App() {
   const { imageSrc, setImageState } = useImage();
@@ -26,13 +28,22 @@ function App() {
   return (
     <div className="flex justify-center items-center bg-primary/20 w-screen h-screen">
       {!imageSrc ? (
-        <Button variant="primary" onClick={openFileDialog} className="relative">
-          <span className="absolute -top-1 -right-1 flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-          </span>
-          Select an image:
-        </Button>
+        <div className="text-center flex flex-col gap-2 justify-center items-center max-w-80">
+          <Button
+            variant="primary"
+            onClick={openFileDialog}
+            className="relative "
+          >
+            <span className="absolute -top-1 -right-1 flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>
+            Select an image :
+          </Button>
+          <p className="text-muted-foreground animate-pulsec">
+            <Balancer>Open an image to start the extract process</Balancer>
+          </p>
+        </div>
       ) : (
         <DataContextProvider>
           <div className="flex overflow-hidden h-full w-full">
